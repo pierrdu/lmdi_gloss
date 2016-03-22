b08adab8e3e05b8f69981fc3a59eb039dc8e2d9b
pierrdu <pierre@duhem.com>
2016-03-22 13:45:35 +0100

    -

---
+++ migrations/release_1_1.php
@@ -0,0 +1,39 @@
+<?php
+/**
+*
+* @package phpBB Extension - LMDI Glossary extension
+* @copyright (c) 2015-2016 Pierre Duhem - LMDI
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace lmdi\gloss\migrations;
+
+class release_1_1 extends \phpbb\db\migration\migration
+{
+
+	public function effectively_installed()
+	{
+		return isset($this->config['lmdi_glossary_tooltip']);
+	}
+
+	static public function depends_on()
+	{
+		return array('\lmdi\gloss\migrations\release_1');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.add', array('lmdi_glossary_tooltip', 50)),
+		);
+	}
+
+	public function revert_data()
+	{
+		return array(
+			array('config.remove', array('lmdi_glossary_tooltip')),
+		);
+	}
+
+}
